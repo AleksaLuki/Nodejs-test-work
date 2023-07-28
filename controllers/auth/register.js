3226b4262808bf7c721d94534efbea49106bc4ab
AleksaLuki <dimirtiy9000@gmail.com>
2023-07-28 12:22:40 +0300

    ava

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -24,49 +24,36 @@
 
 
 
-
-
-
-// const bcrypt = require("bcryptjs");
-// const { User } = require("../../models/user");
-// const { HttpError } = require("../../helpers");
-// const gravatar = require("gravatar");
-
-// const register = async (req, res) => {
-//   const { email, password } = req.body;
-//   const user = await User.findOne({ email });
-
-//     if (user) {
-//       throw HttpError(409, "Email already in use");
-//     }
-
-//     const hashPassword = await bcrypt.hash(password, 10);
-//     const avatarURL = gravatar.url(email);
-//     const newUser = await User.create({ ...req.body, password: hashPassword, avatarURL });
-  
-//     res.status(201)
-//        .json({ email: newUser.email, 
-//              name: newUser.name });
-// };
-
-// module.exports = register;
-
-
-
+const bcrypt = require("bcryptjs");
 const { User } = require("../../models/user");
 const { HttpError } = require("../../helpers");
-
+const gravatar = require("gravatar");
+const { nanoid } = require("nanoid");
+const sendEmail = require("../../helpers/sendEmail");
+const { BASE_URL } = process.env
 
 const register = async (req, res) => {
-  const { email } = req.body;
+  const { email, password } = req.body;
   const user = await User.findOne({ email });
 
     if (user) {
       throw HttpError(409, "Email already in use");
     }
 
-    const newUser = await User.create(req.body);
+    const hashPassword = await bcrypt.hash(password, 10);
+    const avatarURL = gravatar.url(email);
+    const verificationCode = nanoid();
+
+    const newUser = await User.create({ ...req.body, password: hashPassword, avatarURL, verificationCode });
   
+    const verifyEmail = {
+      to: email,
+      subject: "Verify email",
+      html: `<a target="_blank" href="${BASE_URL}/api/auth/verify/${verificationCode}">Click verify email</a>`
+  };
+  
+      await sendEmail(verifyEmail);
+
     res.status(201)
        .json({ email: newUser.email, 
              name: newUser.name });
